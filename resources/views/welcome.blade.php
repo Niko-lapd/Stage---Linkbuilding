5e0bf5490e1c11f96014b6bdf8513ceede47602b
Niko-lapd <54631340+Niko-lapd@users.noreply.github.com>
2020-09-21 15:59:16 +0200

    Sav

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,26 +15,28 @@
 
         <div class="col-md-6 bg-orange account-box text-right">
         	<div class="mr-4">
-				<h4 class="mt-5">Partner Account Aanmaken</h4>
-				<h4>
-					Lorem isum dolor sit amet, consectetuer adiiscing elit.
+			<h6 style="margin-top:40px;"class="mt-5">Adverteerder</h6>
+				<h2 style=" margin-top:0px;" class="mt-5">Laat influencers</h2>
+				<h4 class="mt-5">
+					Over je merk schrijven en 
 				</h4>
-				<h4>
-					Lorem isum dolor sit amet, consectetuer adiiscing elit.
+				<h4 class="mt-5">
+					posten op hun kanaal
 				</h4>
-				<a class="btn btn-primary mb-5" href="/register/partner">Als Partner Registreren</a>
+				<a class="btn btn-primary mb-5, mt-5" href="/register/partner">Ik ben adverteerder</a>
 			</div>
         </div>
         <div class="col-md-6 bg-blue account-box text-left">
         	<div class="ml-4">
-				<h4 class="mt-5">Klant Account Aanmaken</h4>
-				<h4>
-					Lorem isum dolor sit amet, consectetuer adiiscing elit.
+			<h6 style="margin-top:40px;" class="mt-5">Influencers</h6>
+				<h2 style=" margin-top:0px;" class="mt-5">Verdien met content</h2>
+				<h4 class="mt-5">
+					Creatie voor adverteerders
 				</h4>
-				<h4>
-					Lorem isum dolor sit amet, consectetuer adiiscing elit.
+				<h4 class="mt-5">
+					die bij de passen
 				</h4>
-				<a class="btn btn-primary mb-5" href="/register/klant">Als Klant Registreren</a>
+				<a class="btn btn-primary mb-5, mt-5" href="/register/klant">Ik ben influencer</a>
 			</div>
         </div>
         <div class="col-md-12 test-box">
@@ -43,5 +45,4 @@
 </div>
 
 @endsection
-
 
